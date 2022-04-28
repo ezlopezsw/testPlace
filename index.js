b75e4ab40f605ee589292d8042ff1ada01c7f981
Ezequiel Lopez <ezequiel.lopez@southworks.com>
2022-04-28 15:46:19 -0300

    asd

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const testFunction = () => {
   return "Test output";
 };
 
-app.get("/test", (req, res) => rest.send(testFunction()));
+app.get("/test", (req, res) => res.send(testFunction()));
 
 app.listen(5050);
