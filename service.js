4137774e07f3e7c8a10ea8a6a6acf0835a615a1b
Ezequiel Lopez <ezequiel.lopez@southworks.com>
2022-04-28 15:51:54 -0300

    asd

--- service.js
+++ service.js
@@ -2,14 +2,30 @@ const Service = require("node-linux").Service;
 // const { spawn } = require("child_process");
 // const { getArgumentValue } = require("./service/services/argumentReader");
 
+const installationType = "install";
+
 // Create a new service object
 const svc = new Service({
   name: `test`,
   description: `Test Agent`,
   script: "/home/eze/testPlace/index.js",
+  nodeOptions: ["--harmony", "--max_old_space_size=4096"],
+  maxRetries: 0,
+  abortOnError: true,
+  stopparentfirst: false,
 });
 
-svc.start();
+// Stops service and listen for the "Uninstall" event, which indicates the
+// process has stopped and is uninstalled.
+
+svc.stop();
+
+svc.on('uninstall', function () {
+  console.log('Uninstall complete.');
+  console.log('The service exists: ', svc.exists);
+});
+
+svc.uninstall();
 
 svc.on("install", function () {
   console.log("Service installed");
